222c852ae2cf74afb730d4c1c8fc4b73433a5fb9
Austin Benedict <astone@Austins-MBP.attlocal.net>
2020-10-13 09:15:07 -0500

    Finished JS Exercise

---
+++ JavaScript/IntroToJS/JSBasicsExercise/jsBasicsExercise.js
@@ -0,0 +1,54 @@
+console.log(4);
+/* 
+Numbers
+Strings
+Booleans
+Null and Undefined
+*/
+
+// + - * / %
+
+console.log(7%4);
+console.log(3%7);
+
+console.log('I\'M MARY POPPINS, Y\'ALL');
+
+console.log("Spider" + "man");
+
+console.log("Austin".length + "Benedict".length);
+
+console.log("Avengers:Endgame"[15]);
+
+
+// BONUS
+
+console.log("\\");
+
+console.log(5 ** 4 - 9);
+
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+console.log(("I".length + "Am".length * "Iron".length) % "Man".length);
+
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("HAWKEYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+console.log("CAP"[1] + "\n" + "VISION"[0] + "\n" + "HAWKEYE"[4] + "\n" + "NICK FURY"[0] + "\n" + "GROOT"[0] + "\n" + "DOCTOR STRANGE"[13] + "\n" + "THOR"[3] + "\n" + "STAR-LORD"[0]);
+
+var m = 10;
+var c = 5;
+var u = m + c;
+console.log(u);
+
+
+
+
+
+
+
+
+
